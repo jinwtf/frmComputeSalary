d358dc876a342b07df43ce8044c9130706ae1d1b
Jean Dyel Flores <flores.313371@balagtas.sti.edu.ph>
2024-05-24 21:48:03 +0800

    Add project files.

---
+++ EmployeeApplication/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeApplication
+{
+    public partial class Form1 : Form
+    {
+        private PartTimeEmployee partTime;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string FirstName = this.textBox1.Text;
+            string LastName = this.textBox2.Text;
+            string Department = this.textBox3.Text;
+            string JobTitle = this.textBox4.Text;
+            int hoursWorked = Convert.ToInt32(textBox5.Text);
+            int totalhoursWorked = Convert.ToInt32(textBox6.Text);
+
+            partTime = new PartTimeEmployee(FirstName, LastName, Department, JobTitle);
+            partTime.computeSalary(hoursWorked, totalhoursWorked);
+            label8.Text = partTime.FirstName;
+            label10.Text = partTime.LastName;
+            label12.Text = Convert.ToString(partTime.getSalary());
+
+        }
+    }
+    }
---
+++ EmployeeApplication/PartTimeEmployee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApplication
+{
+    internal class PartTimeEmployee : IEmployee
+    {
+        private string first_name;
+        private string last_name;
+        private string department;
+        private string job_title;
+        private double basic_salary;
+        public string FirstName
+        {
+            get { return first_name; }
+            set { first_name = value; }
+        }
+        public string LastName
+        {
+            get { return last_name; }
+            set { last_name = value; }
+        }
+        public string Department
+        {
+            get { return department; }
+            set { department = value; }
+        }
+        public string JobTitle
+        {
+            get { return job_title; }
+            set { job_title = value; }
+        }
+        public double BasicSalary
+        {
+            get { return basic_salary; }
+            set { basic_salary = value; }
+        }
+
+        public PartTimeEmployee(string fName, string lName, string dept, string job_title)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Department = dept;
+            JobTitle = job_title;
+        }
+        public void computeSalary(int hoursWorked, double ratePerHour)
+        {
+            BasicSalary = hoursWorked * ratePerHour;
+        }
+        public double getSalary()
+        {
+            return basic_salary;
+        }
+
+    }
+
+}
